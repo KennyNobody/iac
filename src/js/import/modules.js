ca042da744dbdfa803c886f5d6d30ac4c50bd564
kenny_nobody <nobodykenny@gmail.com>
2020-03-12 15:51:18 +0500

    work

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,2 +1,6 @@
+import {$,jQuery} from 'jquery';
+import fancybox from '@fancyapps/fancybox';
+import select2 from 'select2';
 import "%modules%/header/header";
+import "%modules%/form/form";
 import "%modules%/footer/footer"
